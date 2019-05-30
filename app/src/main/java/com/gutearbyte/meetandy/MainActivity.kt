eb77d0949c55cf5ece9bc01f58f7cb599a418725
Marco Fuhrich <marco.kotz@gmail.com>
2019-05-30 18:51:31 +0200

    Step 4: Invite Andy to the scene

--- app/src/main/java/com/gutearbyte/meetandy/MainActivity.kt
+++ app/src/main/java/com/gutearbyte/meetandy/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.gutearbyte.meetandy
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var andyRenderable: Renderable
+    private lateinit var arFragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        arFragment = ux_fragment as ArFragment
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("Andy.sfb"))
+            .build()
+            .thenAccept { renderable: Renderable -> andyRenderable = renderable }
+            .exceptionally {
+                val toast = Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.show()
+                null
+            }
+
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            // Create the Anchor.
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment.arSceneView.scene)
+
+            // Create the transformable andy and add it to the anchor.
+            val andy = TransformableNode(arFragment.transformationSystem)
+            andy.setParent(anchorNode)
+            andy.renderable = andyRenderable
+            andy.select()
+        }
+
     }
 }
