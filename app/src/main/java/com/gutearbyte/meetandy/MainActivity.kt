8b75206292b99617fdf1575c24a496f0c3e5fa20
Marco Fuhrich <marco.kotz@gmail.com>
2019-05-30 19:21:28 +0200

    Step 6: Get a picture of you and Andy

--- app/src/main/java/com/gutearbyte/meetandy/MainActivity.kt
+++ app/src/main/java/com/gutearbyte/meetandy/MainActivity.kt
@@ -2,7 +2,9 @@ package com.gutearbyte.meetandy
 
 import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.Gravity
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.sceneform.AnchorNode
@@ -11,9 +13,11 @@ import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var andyRenderable: Renderable
     private lateinit var arFragment: PhotoArFragment
+    private var selectedAndy: TransformableNode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,10 +46,34 @@ class MainActivity : AppCompatActivity() {
             val andy = TransformableNode(arFragment.transformationSystem)
             andy.setParent(anchorNode)
             andy.renderable = andyRenderable
+            selectedAndy = andy
             andy.select()
         }
 
-        fabTakePicture.setOnClickListener { arFragment.takePhoto() }
+        fabTakePicture.setOnClickListener { takePhotoWithCountdown() }
+    }
 
+    private fun takePhotoWithCountdown() {
+        object : CountDownTimer(4000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                tvCountdown.text = ((millisUntilFinished / 1000) + 1).toString()
+            }
+
+            override fun onFinish() {
+                tvCountdown.text = "Cheese!"
+                tvCountdown.postDelayed({
+                    tvCountdown.visibility = View.INVISIBLE
+                    fabTakePicture.show()
+                    arFragment.arSceneView.planeRenderer.isEnabled = true
+                }, 1000)
+                arFragment.takePhoto()
+            }
+        }.start()
+        // Hide all the chrome for a nice photo
+        arFragment.arSceneView.planeRenderer.isEnabled = false
+        tvCountdown.visibility = View.VISIBLE
+        fabTakePicture.hide()
+        selectedAndy?.transformationSystem?.selectNode(null)
     }
 }
